ed72e7820f8d46026d06db30ca6d79149d836d49
Ishii Koto <takuminn717@gmail.com>
2020-12-25 14:03:52 +0900

    リポジトリ作成

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {url: "ここにリンクがくるほげ",}
+  }
+
+  randomInt(min,max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+  handleClick(){
+    const contest = this.randomInt(126,187);
+    const problem = String.fromCharCode(this.randomInt(0,3) + 'a'.charCodeAt(0));
+    console.log("コンテスト:" + contest + " 問題:" + problem);
+
+    this.setState({url: "https://atcoder.jp/contests/abc" + contest + "/tasks/abc" + contest + "_" + problem});
+  }
+
+  render(){
+    return(
+      <dev>
+        <a href={this.state.url}>{this.state.url}</a>
+        <button onClick={() => {this.handleClick()}}>リンク生成</button>
+      </dev>
+    );
+  }
 }
 
 export default App;
